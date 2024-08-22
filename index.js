40bc1cc611ac6fce085e17edf0ace0fb91c6b9d6
joshmorenx <joshuamoreno21@gmail.com>
2024-08-22 13:22:14 -0600

    Cambios para que .env tenga la ruta variable por si ocurren cambios

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const serveIndex = require('serve-index');
 const path = require('path');
 
 const app = express();
-const port = 80;
+const port = 4000;
+require('dotenv').config();
 
-const directoryPathFotoTablet = path.join('Z:','1FotoTablet');
-const directoryPathFotoAcce = path.join('Z:','1FotoAcce');
+const directoryPathFotoTablet = path.join(process.env.SELECTED_PATH, '1FotoTablet');
+const directoryPathFotoAcce = path.join(process.env.SELECTED_PATH, '1FotoAcce');
 
 const routeTablet = '/1FotoTablet';
 const routeAcce = '/1FotoAcce';
@@ -17,15 +18,15 @@ app.use(routeAcce, express.static(directoryPathFotoAcce));
 
 // app.use('/', serveIndex(directoryPathFotoTablet, { icons: true, view: 'details'}));
 
-app.use(routeTablet, serveIndex(directoryPathFotoTablet, { icons: true, view: 'details'}));
-app.use(routeAcce, serveIndex(directoryPathFotoAcce, { icons: true, view: 'details'}));
+app.use(routeTablet, serveIndex(directoryPathFotoTablet, { icons: true, view: 'details' }));
+app.use(routeAcce, serveIndex(directoryPathFotoAcce, { icons: true, view: 'details' }));
 
 
-app.get('/', (req, res) => {
-    res.send('Ruta raíz accesible');
+app.use('/', (req, res) => {
+    res.send('<h1>FotoProduServ</h1><div style=" font-family: Arial, Helvetica, sans-serif; text-align: center; margin-top: 10px;"> <ol> <li style="margin-bottom: 5vh;"><a style="font-size: 5vw;" href="/1FotoTablet">1FotoTablet</a></li>  <li><a style="font-size: 5vw;" href="/1FotoAcce">1FotoAcce</a> </ol> </li> </div>');
 });
 
 app.listen(port, () => {
+    console.log('Es necesario cersiorarse de que .env está configurado');
     console.log(`Servidor escuchando en http://localhost:${port}`);
-    console.log('Cersiorarse de que la unidad de red Z: esté montada');
 });
